828683ca8b250fcf0df0f6f19de63f0bc6dcfa73
hudashulaiba <56040765+hudashulaiba@users.noreply.github.com>
2021-09-29 17:44:23 -0400

    Update hw4.py
    methods and main mostly done, just need testing

--- hw4.py
+++ hw4.py
@@ -1,5 +1,6 @@
 
 import unittest
+import random #FOR EXTRA CREDIT
 
 # The Customer class
 # The Customer class represents a customer who will order from the stalls.
@@ -27,8 +28,13 @@ class Customer:
     
     # Submit_order takes a cashier, a stall and an amount as parameters, 
     # it deducts the amount from the customer’s wallet and calls the receive_payment method on the cashier object
+    #TO DO
     def submit_order(self, cashier, stall, amount): 
-        pass
+        self.wallet = self.wallet - amount
+        cashier.receive_payment(stall, amount)
+        #EXTRA CREDIT
+        if cashier.lucky():
+            self.wallet = self.wallet + 10
 
     # The __str__ method prints the customer's information.    
     def __str__(self):
@@ -40,9 +46,25 @@ class Customer:
 class Cashier:
 
     # Constructor
-    def __init__(self, name, directory =[]):
+    def __init__(self, name, directory =[], orders = 0): #add list of customers for EC
         self.name = name
         self.directory = directory[:] # make a copy of the directory
+        self.orders = orders #for extra credit
+
+    #EXTRA CREDIT
+    #for each 10th order, run lucky draw w 5% probability of customer getting $10
+    def lucky(self):
+        #keep track of order
+        self.orders+=1
+
+        #if it's the 10th
+        if self.orders % 10 == 0:
+            if random.randrange(0,20) == 1:
+                #added in validate order customer gets 10 bucks if true
+                return True
+            else:
+                return False
+    
 
     # Whether the stall is in the cashier's directory
     def has_stall(self, stall):
@@ -55,6 +77,8 @@ class Cashier:
     # Receives payment from customer, and adds the money to the stall's earnings.
     def receive_payment(self, stall, money):
         stall.earnings += money
+        #FOR EXTRA CREDIT
+        self.lucky()
 
     # Places an order at the stall.
 	# The cashier pays the stall the cost.
@@ -70,9 +94,47 @@ class Cashier:
         return "Hello, this is the " + self.name + " cashier. We take preloaded market payment cards only. We have " + str(sum([len(category) for category in self.directory.values()])) + " vendors in the farmers' market."
 
 ## Complete the Stall class here following the instructions in HW_4_instructions_rubric
+#TO DO
 class Stall:
-    
-    pass
+
+    #initialize, defaults: cost - 7, earnings - 0
+    def __init__(self, name, inventory, cost = 7, earnings = 0):
+        self.name = name
+        self.inventory = inventory.copy()
+        self.cost = cost
+        self.earnings = earnings
+
+    def __str__(self):
+        keyslist = str(self.inventory.keys())
+        return("Hello, we are " + self.name + ". This is the current menu " + keyslist + ". We charge $" + str(self.cost) + 
+                " per item. We have $" + str(self.earnings) + " in total.")
+
+    #takes food name and quantity
+    def process_order(self, name, quantity):
+        #decrease quantity from stall's inventory
+        self.inventory[name] = self.inventory[name] - quantity
+
+    def has_item(self, name, quantity):
+        #check if food is in the directory
+        if name in self.inventory:
+            #true if enough food is left
+            if quantity <= self.inventory[name]:
+                return True
+        #otherwise return false
+        return False
+
+    def stock_up(self, name, quantity):
+        #add quantity to existing quantity if item in dictionary
+        if name in self.inventory:
+            self.inventory[name] = self.inventory[name] + quantity
+        else:
+            #create new item in dictionary
+            self.inventory[name] = quantity
+
+    def compute_cost(self, quantity):
+        #return total for order
+        return quantity * self.cost
+
 
 
 class TestAllMethods(unittest.TestCase):
@@ -83,7 +145,7 @@ class TestAllMethods(unittest.TestCase):
         self.f2 = Customer("Morgan", 150)
         self.s1 = Stall("The Grill Queen", inventory, cost = 10)
         self.s2 = Stall("Tamale Train", inventory, cost = 9)
-        self.s3 = Stall("The Streatery", inventory)
+        self.s3 = Stall("The Streatery", inventory) #default cost = 7
         self.c1 = Cashier("West")
         self.c2 = Cashier("East")
         #the following codes show that the two cashiers have the same directory
@@ -105,7 +167,7 @@ class TestAllMethods(unittest.TestCase):
         self.assertEqual(len(self.c1.directory), 3) 
 
 	## Check to see whether constructors work
-    def test_truck_constructor(self):
+    def test_stall_constructor(self):
         self.assertEqual(self.s1.name, "The Grill Queen")
         self.assertEqual(self.s1.inventory, {"Burger":40, "Taco":50})
         self.assertEqual(self.s3.earnings, 0)
@@ -146,9 +208,9 @@ class TestAllMethods(unittest.TestCase):
 	# Test that computed cost works properly.
     def test_compute_cost(self):
         #what's wrong with the following statements?
-        #can you correct them?
-        self.assertEqual(self.s1.compute_cost(self.s1,5), 51)
-        self.assertEqual(self.s3.compute_cost(self.s3,6), 45)
+        #can you correct them? DONE
+        self.assertEqual(self.s1.compute_cost(5), 50) #ISSUE: cost should be 50 (5*10), not 51
+        self.assertEqual(self.s3.compute_cost(6), 42) #ISSUE: cost should be 42 (7*6), not 45
 
 	# Check that the stall can properly see when it is empty
     def test_has_item(self):
@@ -178,17 +240,44 @@ class TestAllMethods(unittest.TestCase):
     
 ### Write main function
 def main():
-    #Create different objects 
+    #Create different objects
+
+    #at least 2 dictionaries w 3 types of food (food name and quantity) 
+    i1 = {"Pizza":35, "Salad":70, "Pasta":12}
+    i2 = {"Bread":89, "Sandwich":18, "Quesadilla":63, "Rice":109}
+
+    #3 customer objects unique name and wallet amount
+    huda = Customer("Huda") #default wallet = 100
+    mike = Customer("Mike", 703)
+    helen = Customer("Helen", 89)
+    ann = Customer("Ann", 35)
+
+    #2 stall objects w/ unique name and cost. use created inventories
+    s1 = Stall("Italian", i1, 12)
+    s2 = Stall("Carb City", i2, 8)
+
+    #2 cashier objects w/ unique name and directory
+    c1 = Cashier("Tyler", [s1])
+    c2 = Cashier("Frankie", [s2])
 
     #Try all cases in the validate_order function
     #Below you need to have *each customer instance* try the four cases
     #case 1: the cashier does not have the stall 
+    mike.validate_order(c1, s2, "Bread", 2)
+    print("EXPECTED: Sorry, we don't have that vendor stall. Please try a different one. \n")
     
     #case 2: the casher has the stall, but not enough ordered food or the ordered food item
+    helen.validate_order(c2, s2, "Salad", 2)
+    print("EXPECTED: Our stall has run out of Salad :( Please try a different stall!\n")
+    helen.validate_order(c1, s1, "Pasta", 13)
+    print("EXPECTED: Our stall has run out of Pasta :( Please try a different stall!\n")
     
-    #case 3: the customer does not have enough money to pay for the order: 
+    #case 3: the customer does not have enough money to pay for the order:
+    ann.validate_order(c2, s2, "Rice", 5)
+    print("EXPECTED: Don't have enough money for that :( Please reload more money!\n") 
     
     #case 4: the customer successfully places an order
+    huda.validate_order(c2, s2, "Quesadilla", 3) 
 
     pass
 
